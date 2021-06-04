b7bf72d05035f857b1fc989010ede37f04b0caf3
Tarxos <ftarxos@gmail.com>
2021-06-05 00:53:52 +0300

    Converter base

---
+++ Thundire.MVVM.WPF/Converters/Base/ConverterBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Thundire.MVVM.WPF.Converters.Base
+{
+    public abstract class ConverterBase : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotSupportedException("Reverse conversion is not supported");
+    }
+}
