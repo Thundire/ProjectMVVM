ee4dac2857a11ab124a584cef9c86895c2835fec
Tarxos <ftarxos@gmail.com>
2021-06-05 00:53:33 +0300

    Notify Base for view models

---
+++ Thundire.MVVM.WPF/Observable/Base/SetInstance{TValue}.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Thundire.MVVM.WPF.Observable.Base
+{
+    public readonly ref struct SetInstance<T>
+    {
+        public SetInstance(bool success, T previous, T newValue)
+        {
+            State = new(success, previous, newValue);
+        }
+
+        public SetInstanceState State { get; init; }
+
+        public SetInstance<T> Do(Action<SetInstanceState> behavior)
+        {
+            behavior?.Invoke(State);
+            return this;
+        }
+
+        public SetInstance<T> DoOnSuccess(Action<SetInstanceState> behavior)
+        {
+            if (!State.Success) return this;
+            behavior?.Invoke(State);
+            return this;
+        }
+
+        public SetInstance<T> DoOnFailure(Action<SetInstanceState> behavior)
+        {
+            if (State.Success) return this;
+            behavior?.Invoke(State);
+            return this;
+        }
+
+        public readonly struct SetInstanceState
+        {
+            public SetInstanceState(bool success, T previous, T newValue)
+            {
+                Success = success;
+                Previous = previous;
+                New = newValue;
+            }
+
+            public bool Success { get; }
+            public T Previous { get; }
+            public T New { get; }
+        }
+    }
+
+    
+}
