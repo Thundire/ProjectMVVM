321bc3abe9b94d8e993cbcd96ea6bf36b0744c89
Tarxos <ftarxos@gmail.com>
2021-06-05 00:55:01 +0300

    horizontal offset converter

---
+++ Thundire.MVVM.WPF/Converters/HorizontalOffsetConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Thundire.MVVM.WPF.Converters
+{
+    public class HorizontalOffsetConverter : Freezable, IValueConverter
+    {
+        public static readonly DependencyProperty ParentActualWidthProperty = DependencyProperty.Register(
+            nameof(ParentActualWidth), typeof(double), typeof(HorizontalOffsetConverter), new PropertyMetadata(default(double)));
+        public double ParentActualWidth
+        {
+            get => (double) GetValue(ParentActualWidthProperty);
+            set => SetValue(ParentActualWidthProperty, value);
+        }
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double width) return default;
+            return ParentActualWidth - width;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+
+        protected override Freezable CreateInstanceCore() => new HorizontalOffsetConverter(){ParentActualWidth = ParentActualWidth};
+    }
+}
