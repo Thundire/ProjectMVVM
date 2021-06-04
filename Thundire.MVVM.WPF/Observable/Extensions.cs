84296600cb7d9b7bb45378d8a2a6306068cb8bf0
Tarxos <ftarxos@gmail.com>
2021-06-05 00:56:48 +0300

    observable collection extension to add items with dispatcher

---
+++ Thundire.MVVM.WPF/Observable/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Thundire.MVVM.WPF.Observable
+{
+    public static class Extensions
+    {
+        public static void DispatcherAdd<T>(this ObservableCollection<T> self, T item)
+        {
+            Application.Current.Dispatcher.BeginInvoke( (Action)delegate { self.Add(item); });
+        }
+    }
+}
