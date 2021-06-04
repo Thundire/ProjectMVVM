60538e2c1cc79176b2ab891db3692aafa9620621
Tarxos <ftarxos@gmail.com>
2021-06-05 00:55:26 +0300

    scroll to selected item behavior

---
+++ Thundire.MVVM.WPF/Behaviors/ScrollIntoViewSelectedItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace Thundire.MVVM.WPF.Behaviors
+{
+    public class ScrollIntoViewSelectedItemBehavior : Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.SelectionChanged += ScrollToSelected;
+            AssociatedObject.Loaded += ScrollToSelected;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.SelectionChanged -= ScrollToSelected;
+            AssociatedObject.Loaded -= ScrollToSelected;
+        }
+        
+        private static void ScrollToSelected(object sender, RoutedEventArgs e)
+        {
+            var box = sender as ListBox;
+            box?.ScrollIntoView(box.SelectedItem);
+        }
+    }
+}
