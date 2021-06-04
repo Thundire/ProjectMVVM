e8a1244f91bc4776bfce3651a53078f8700498dd
Tarxos <ftarxos@gmail.com>
2021-06-05 00:55:39 +0300

    task competions

---
+++ Thundire.MVVM.WPF/Observable/TaskCompletion/NotifyTaskCompletion{TResult}.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Thundire.MVVM.WPF.Observable.TaskCompletion
+{
+    public class NotifyTaskCompletion<TResult> : INotifyPropertyChanged
+    {
+        public NotifyTaskCompletion(Task<TResult> task)
+        {
+            Task = task;
+            if (!task.IsCompleted)
+            {
+                var _ = WatchTaskAsync(task);
+            }
+        }
+
+        private async Task WatchTaskAsync(Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch { }
+
+            var propertyChanged = PropertyChanged;
+            if (propertyChanged == null) return;
+            propertyChanged(this, new(nameof(Status)));
+            propertyChanged(this, new(nameof(IsCompleted)));
+            propertyChanged(this, new(nameof(IsNotCompleted)));
+            if (task.IsCanceled)
+            {
+                propertyChanged(this, new(nameof(IsCanceled)));
+            }
+            else if (task.IsFaulted)
+            {
+                propertyChanged(this, new(nameof(IsFaulted)));
+                propertyChanged(this, new(nameof(Exception)));
+                propertyChanged(this, new(nameof(InnerException)));
+                propertyChanged(this, new(nameof(ErrorMessage)));
+            }
+            else
+            {
+                propertyChanged(this, new(nameof(IsSuccessfullyCompleted)));
+                propertyChanged(this, new(nameof(Result)));
+            }
+        }
+
+        public Task<TResult> Task { get; }
+        public TResult Result => (Task.Status == TaskStatus.RanToCompletion) ? Task.Result : default;
+        public TaskStatus Status => Task.Status;
+        public bool IsCompleted => Task.IsCompleted;
+        public bool IsNotCompleted => !Task.IsCompleted;
+
+        public bool IsSuccessfullyCompleted => Task.Status == TaskStatus.RanToCompletion;
+
+        public bool IsCanceled => Task.IsCanceled;
+        public bool IsFaulted => Task.IsFaulted;
+
+        public AggregateException Exception => Task.Exception;
+        public Exception InnerException => Exception?.InnerException;
+        public string ErrorMessage => InnerException?.Message;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
