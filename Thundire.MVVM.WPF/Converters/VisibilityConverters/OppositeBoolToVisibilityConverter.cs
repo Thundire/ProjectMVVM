b53d9faa3bb065628783b2f875d73989d4de1de0
Tarxos <ftarxos@gmail.com>
2021-06-05 00:54:40 +0300

    visibility converters

---
+++ Thundire.MVVM.WPF/Converters/VisibilityConverters/OppositeBoolToVisibilityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using Thundire.MVVM.WPF.Converters.Base;
+
+namespace Thundire.MVVM.WPF.Converters.VisibilityConverters
+{
+    public class OppositeBoolToVisibilityConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool result)
+                return !result ? Visibility.Visible : Visibility.Collapsed;
+            return Visibility.Collapsed;
+        }
+    }
+}
---
+++ Thundire.MVVM.WPF/Converters/VisibilityConverters/OppositeNullableVisibilityConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using Thundire.MVVM.WPF.Converters.Base;
+
+namespace Thundire.MVVM.WPF.Converters.VisibilityConverters
+{
+    public class OppositeNullableVisibilityConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is { } ? Visibility.Collapsed : Visibility.Visible;
+    }
+}
