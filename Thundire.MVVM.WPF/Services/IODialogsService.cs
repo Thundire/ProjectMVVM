68fc760736a19340cc990303bd46d0bc8158b353
Tarxos <ftarxos@gmail.com>
2021-06-05 00:57:07 +0300

    IO dialogs service

---
+++ Thundire.MVVM.WPF/Services/IODialogsService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using Thundire.MVVM.WPF.Services.Interfaces;
+
+namespace Thundire.MVVM.WPF.Services
+{
+    public class IODialogsService : IIODialogsService
+    {
+        public bool SaveFile(string title, out string selectedFile, string defaultFileName = null, string filter = "All files (*.*)|*.*")
+        {
+            var dlg = new SaveFileDialog
+            {
+                Title = title,
+                Filter = filter
+            };
+
+            if (!string.IsNullOrWhiteSpace(defaultFileName)) dlg.FileName = defaultFileName;
+
+            if (dlg.ShowDialog() is not true)
+            {
+                selectedFile = null;
+                return false;
+            }
+
+            selectedFile = dlg.FileName;
+            return true;
+        }
+
+        public bool OpenFile(string title, out string selectedFile, string filter = "All files (*.*)|*.*")
+        {
+            var dlg = new OpenFileDialog
+            {
+                Title = title,
+                Filter = filter
+            };
+
+            if (dlg.ShowDialog() is not true)
+            {
+                selectedFile = null;
+                return false;
+            }
+
+            selectedFile = dlg.FileName;
+            return true;
+        }
+    }
+}
---
+++ Thundire.MVVM.WPF/Services/Interfaces/IIODialogsService.cs
@@ -0,0 +1,8 @@
+﻿namespace Thundire.MVVM.WPF.Services.Interfaces
+{
+    public interface IIODialogsService
+    {
+        bool SaveFile(string title, out string selectedFile, string defaultFileName = null, string filter = "All files (*.*)|*.*");
+        bool OpenFile(string title, out string selectedFile, string filter = "All files (*.*)|*.*");
+    }
+}
