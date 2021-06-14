d93b85b4b988bba0448019135ceb757bd4ca03b5
Tarxos <ftarxos@gmail.com>
2021-06-14 19:56:43 +0300

    inject autofac

--- Samples/AutofacSample/App.xaml.cs
+++ Samples/AutofacSample/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows;
+﻿using Autofac;
+
+using System.Windows;
 
 namespace AutofacSample
 {
     public partial class App : Application
     {
+        public static ILifetimeScope Services { get; } = AutofacContainer.CreateSimpleContainer().Build();
     }
 }
