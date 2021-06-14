da32ec63f006a64e7bce0e5df789d6f11c654b4b
Tarxos <ftarxos@gmail.com>
2021-06-14 12:42:06 +0300

    replace DataTemplates register interfaces, add internal implementation

--- Thundire.MVVM.WPF/Services/Interfaces/ITemplatesCacheBuilder.cs
+++ Thundire.MVVM.WPF/Services/Interfaces/ITemplatesCacheBuilder.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Thundire.MVVM.WPF.Services.ViewService.Regions.Interfaces
+namespace Thundire.MVVM.WPF.Services.Interfaces
 {
     public interface ITemplatesCacheBuilder
     {
