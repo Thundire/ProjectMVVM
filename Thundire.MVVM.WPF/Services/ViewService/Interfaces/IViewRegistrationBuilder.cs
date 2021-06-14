4835c70c5cde771f9eb07ce8088ecae4e5eb31c2
Tarxos <ftarxos@gmail.com>
2021-06-14 17:54:58 +0300

    clear code in view service

---
+++ Thundire.MVVM.WPF/Services/ViewService/Interfaces/IViewRegistrationBuilder.cs
@@ -0,0 +1,13 @@
+﻿namespace Thundire.MVVM.WPF.Services.ViewService.Interfaces
+{
+    public interface IViewRegistrationBuilder
+    {
+        IViewRegistration AsRegistration();
+        IViewRegistrationBuilder MarkAs(string mark);
+        IViewRegistrationBuilder ViewAsSingleton();
+        IViewRegistrationBuilder ViewAsTransient();
+        IViewRegistrationBuilder ViewModelAsSingleton();
+        IViewRegistrationBuilder ViewModelAsTransient();
+        IViewRegistrationBuilder WithViewModel<TViewModel>();
+    }
+}
--- Thundire.MVVM.WPF/Services/ViewService/ViewRegister.cs
+++ Thundire.MVVM.WPF/Services/ViewService/ViewRegister.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Thundire.MVVM.WPF.Services.Interfaces;
 using Thundire.MVVM.WPF.Services.ViewService.Interfaces;
 using Thundire.MVVM.WPF.Services.ViewService.Models;
 
@@ -7,28 +7,25 @@ namespace Thundire.MVVM.WPF.Services.ViewService
 {
     public class ViewRegister : IViewRegister, IViewRegisterCache
     {
-        private readonly IDIContainerBuilder _builder;
+        private readonly IDIContainerBuilder _container;
         private readonly List<ViewRegistration> _cache = new();
-        private readonly List<ViewRegistrationBuilder> _builders = new();
+        private readonly List<IViewRegistration> _builders = new();
 
-        public ViewRegister(IDIContainerBuilder builder)
-        {
-            _builder = builder;
-        }
-        
+        public ViewRegister(IDIContainerBuilder builder) => _container = builder;
+
         public IReadOnlyList<ViewRegistration> Registrations => _cache;
         
-        public ViewRegistrationBuilder Register<TView>(string mark) where TView : IView
+        public IViewRegistrationBuilder Register<TView>(string mark) where TView : IView
         {
             var builder = new ViewRegistrationBuilder(typeof(TView)).MarkAs(mark);
-            _builders.Add(builder);
+            _builders.Add(builder.AsRegistration());
             return builder;
         }
 
-        public ViewRegistrationBuilder Register<TView, TViewModel>(string mark) where TView : IView
+        public IViewRegistrationBuilder Register<TView, TViewModel>(string mark) where TView : IView
         {
             var builder = new ViewRegistrationBuilder(typeof(TView)).MarkAs(mark).WithViewModel<TViewModel>();
-            _builders.Add(builder);
+            _builders.Add(builder.AsRegistration());
             return builder;
         }
 
@@ -36,64 +33,10 @@ namespace Thundire.MVVM.WPF.Services.ViewService
         {
             foreach (var builder in _builders)
             {
-                _builder.RegisterType(builder.View, builder.ViewMode);
-                _builder.RegisterType(builder.ViewModel, builder.ViewModelMode);
+                _container.RegisterType(builder.View, builder.ViewMode);
+                _container.RegisterType(builder.ViewModel, builder.ViewModelMode);
                 _cache.Add(builder.Build());
             }
-        }
-        
-        public class ViewRegistrationBuilder
-        {
-            internal Type View { get; }
-            internal Type ViewModel { get; private set; }
-            private string Mark { get; set; }
-            internal LifeTimeMode ViewMode { get; private set; } = LifeTimeMode.Transient;
-            internal LifeTimeMode ViewModelMode { get; private set; } = LifeTimeMode.Transient;
-
-            public ViewRegistrationBuilder(Type view)
-            {
-                if (!view.IsAssignableTo(typeof(IView)))
-                    throw new ArgumentException("View must inherit from IView");
-                View = view;
-            }
-
-            public ViewRegistrationBuilder WithViewModel<TViewModel>()
-            {
-                ViewModel = typeof(TViewModel);
-                return this;
-            }
-
-            public ViewRegistrationBuilder MarkAs(string mark)
-            {
-                Mark = mark;
-                return this;
-            }
-
-            public ViewRegistrationBuilder ViewAsSingleton()
-            {
-                ViewMode = LifeTimeMode.Singleton;
-                return this;
-            }
-
-            public ViewRegistrationBuilder ViewAsTransient()
-            {
-                ViewMode = LifeTimeMode.Transient;
-                return this;
-            }
-
-            public ViewRegistrationBuilder ViewModelAsSingleton()
-            {
-                ViewModelMode = LifeTimeMode.Singleton;
-                return this;
-            }
-
-            public ViewRegistrationBuilder ViewModelAsTransient()
-            {
-                ViewModelMode = LifeTimeMode.Transient;
-                return this;
-            }
-
-            internal ViewRegistration Build() => new(Mark, View, ViewModel);
         }
     }
 }
