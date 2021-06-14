d27195b56a401f0f848c1dda10d3a81bd5948f7b
Tarxos <ftarxos@gmail.com>
2021-06-14 17:54:27 +0300

    add extension for observable collection to add range of items

--- Thundire.MVVM.WPF/Observable/Extensions.cs
+++ Thundire.MVVM.WPF/Observable/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 
@@ -9,6 +10,14 @@ namespace Thundire.MVVM.WPF.Observable
         public static void DispatcherAdd<T>(this ObservableCollection<T> self, T item)
         {
             Application.Current.Dispatcher.BeginInvoke( (Action)delegate { self.Add(item); });
+        }
+
+        public static void AddRange<T>(this ObservableCollection<T> self, IEnumerable<T> range)
+        {
+            foreach (var item in range)
+            {
+                self.Add(item);
+            }
         }
     }
 }
