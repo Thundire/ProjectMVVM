bfd3279d1780bf915990e0e84737a326547a6f9c
Tarxos <ftarxos@gmail.com>
2021-06-15 12:18:13 +0300

    inject autofac

--- Samples/AutofacSample/App.xaml.cs
+++ Samples/AutofacSample/App.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System.Windows;
+﻿using Autofac;
+using AutofacSample.SubCode;
+
+using System.Windows;
 
 namespace AutofacSample
 {
     public partial class App : Application
     {
+        public static ILifetimeScope Services { get; } = ContainerConfiguration.CreateSimpleContainer().Build();
     }
 }
---
+++ Samples/AutofacSample/SubCode/ContainerConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+
+namespace AutofacSample.SubCode
+{
+    public static class ContainerConfiguration
+    {
+        // use this method if you need to inject autofac in Microsoft Dependency injection 
+        // usage:
+        // public static IHostBuilder CreateHostBuilder(string[] args) => Host
+        //       .CreateDefaultBuilder(args)
+        //       .UseServiceProviderFactory(ContainerConfiguration.GetFactory)
+        //
+        // public static IServiceProviderFactory<ContainerBuilder> GetFactory(HostBuilderContext context)
+        // {
+        //     return new AutofacServiceProviderFactory(builder => builder.ConfigureContainerServices());
+        // }
+
+        public static ContainerBuilder CreateSimpleContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.ConfigureContainerServices();
+
+            return builder;
+        }
+
+        private static void ConfigureContainerServices(this ContainerBuilder builder)
+            => builder
+                .RegisterServices()
+                .RegisterViewModels()
+                .RegisterViews()
+        ;
+
+        private static ContainerBuilder RegisterServices(this ContainerBuilder builder)
+        {
+            return builder;
+        }
+
+        private static ContainerBuilder RegisterViewModels(this ContainerBuilder builder)
+        {
+            return builder;
+        }
+
+        private static ContainerBuilder RegisterViews(this ContainerBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
