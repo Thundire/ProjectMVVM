7aedf6f4a27fe960359204bf279876df382fd797
Tarxos <ftarxos@gmail.com>
2021-06-28 01:52:59 +0300

    Edit Form base

---
+++ Thundire.MVVM.WPF/Observable/EditForm/EditFormBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+using Thundire.MVVM.WPF.Commands.Relay;
+using Thundire.MVVM.WPF.Observable.Base;
+
+namespace Thundire.MVVM.WPF.Observable.EditForm
+{
+    public abstract class EditFormBase : NotifyBase
+    {
+        protected EditFormBase()
+        {
+            CloseFormCommand = new RelayCommand(() => EndWork(new() { Result = false }));
+        }
+
+        public event EventHandler<EditFormResultArgs> OnWorkDone;
+
+        public ICommand ConfirmCommand { get; init; }
+        public ICommand CancelCommand { get; init; }
+
+        public ICommand CloseFormCommand { get; }
+
+        protected virtual void EndWork(EditFormResultArgs result)
+        {
+            OnWorkDone?.Invoke(this, result);
+        }
+    }
+}
---
+++ Thundire.MVVM.WPF/Observable/EditForm/EditFormResultArgs.cs
@@ -0,0 +1,7 @@
+﻿namespace Thundire.MVVM.WPF.Observable.EditForm
+{
+    public class EditFormResultArgs
+    {
+        public bool Result { get; init; }
+    }
+}
