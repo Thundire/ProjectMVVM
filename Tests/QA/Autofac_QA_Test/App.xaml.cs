9a98497852f086ef7722958e404e421d62b75b86
Tarxos <ftarxos@gmail.com>
2021-06-29 00:23:30 +0300

    add QA test project for Autofac

---
+++ Tests/QA/Autofac_QA_Test/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Autofac_QA_Test
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Tests/QA/Autofac_QA_Test/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Autofac_QA_Test
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
