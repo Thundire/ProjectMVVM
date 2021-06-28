cd749adfa94d0c6612f163292f741f3bf8c98351
Tarxos <ftarxos@gmail.com>
2021-06-29 00:44:40 +0300

    Add container configuration, and view keys

--- Tests/QA/Autofac_QA_Test/App.xaml.cs
+++ Tests/QA/Autofac_QA_Test/App.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System.Windows;
+﻿using Autofac;
+
+using System.Windows;
+using Autofac_QA_Test.AppConfiguration;
 
 namespace Autofac_QA_Test
 {
     public partial class App : Application
     {
+        public static ILifetimeScope Services { get; } = ContainerConfiguration.CreateSimpleContainer().Build();
     }
 }
---
+++ Tests/QA/Autofac_QA_Test/AppConfiguration/ContainerConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+
+namespace Autofac_QA_Test.AppConfiguration
+{
+    public static class ContainerConfiguration
+    {
+        public static ContainerBuilder CreateSimpleContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterServices()
+                .RegisterViewModels()
+                .RegisterViews();
+
+            return builder;
+        }
+
+        private static ContainerBuilder RegisterServices(this ContainerBuilder builder)
+        {
+            return builder;
+        }
+
+        private static ContainerBuilder RegisterViewModels(this ContainerBuilder builder)
+        {
+            return builder;
+        }
+
+        private static ContainerBuilder RegisterViews(this ContainerBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
