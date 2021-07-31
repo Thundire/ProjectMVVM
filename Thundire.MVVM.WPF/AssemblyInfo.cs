80422689b96215183ba8d2857929eca9d762b67b
Tarxos <ftarxos@gmail.com>
2021-07-31 12:58:58 +0300

    update packages, and connect tests project to library Thundire.MVVM.WPF

--- Thundire.MVVM.WPF/AssemblyInfo.cs
+++ Thundire.MVVM.WPF/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +10,7 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+[assembly:InternalsVisibleTo("Thundire.MVVM.WPF.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fd4d646c" +
+                             "3367dfe31c9d113e7f788b9db540b2177f766b1bcc835a67e83cee2c6a203cf09914a671efd8033e179ae3e9a87692b003eba3fb869d2d3e" +
+                             "54d20e0a65b54fc0a6b572be3caca4fc5b1efe469921da8f737fb768b2c9cae0d40cf6b6a68f01d1f0c2ecad571daaf6e905cfe96d083cb70" +
+                             "e116442ec68e841735660dc", AllInternalsVisible = true)]
