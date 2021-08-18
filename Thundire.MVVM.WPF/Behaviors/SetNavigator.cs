1b315153e669750bc40b7977767c90ec08be44d1
Tarxos <ftarxos@gmail.com>
2021-08-18 13:28:06 +0300

    add missed SetNavigator behavior, to set to INavigator Navigation Service from Frame

---
+++ Thundire.MVVM.WPF/Behaviors/SetNavigator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows;
+
+using Thundire.MVVM.WPF.Services.Navigator.Interfaces;
+
+namespace Thundire.MVVM.WPF.Behaviors
+{
+    public class SetNavigator : Behavior<Frame>
+    {
+        public static readonly DependencyProperty NavigatorProperty = DependencyProperty.Register(
+            nameof(Navigator),
+            typeof(INavigationHandler),
+            typeof(SetNavigator),
+            new(default(INavigationHandler), NavigatorChanged));
+
+        public INavigationHandler Navigator { get => (INavigationHandler)GetValue(NavigatorProperty); set => SetValue(NavigatorProperty, value); }
+
+        private NavigationService _navigator;
+
+        protected override void OnAttached()
+        {
+            _navigator = AssociatedObject.NavigationService;
+        }
+
+        protected override void OnDetaching()
+        {
+            Navigator.NavigationService = null;
+            _navigator = null;
+        }
+
+        private static void NavigatorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not SetNavigator behavior || e.NewValue is not INavigationHandler next) return;
+            if (e.OldValue is INavigationHandler prev) prev.NavigationService = null;
+            next.NavigationService = behavior._navigator;
+        }
+    }
+}
