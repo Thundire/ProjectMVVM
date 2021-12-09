b3ff91ff761616ffbd80cfa45ddc474e8d4b53f8
Tarxos <ftarxos@gmail.com>
2021-12-09 17:02:13 +0300

    add extension that create ObservableCollection from enumeration

---
+++ Thundire.MVVM.Core/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Thundire.MVVM.Core
+{
+    public static class Extensions
+    {
+        public static ObservableCollection<T> ToObservable<T>(this IEnumerable<T> param)
+            => param is not null ? new(param) : new();
+    }
+}
