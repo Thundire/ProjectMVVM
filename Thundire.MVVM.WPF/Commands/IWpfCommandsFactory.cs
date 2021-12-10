bc8598156674d139d8be6fc87ac13352c0668354
Tarxos <ftarxos@gmail.com>
2021-12-10 15:55:02 +0300

    remove base abstraction of ICommandsFactory because it not used and hided with IWpfCommand implementation

--- Thundire.MVVM.WPF/Commands/IWpfCommandsFactory.cs
+++ Thundire.MVVM.WPF/Commands/IWpfCommandsFactory.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Thundire.MVVM.Core.Services;
 
 namespace Thundire.MVVM.WPF.Commands
 {
-    public interface IWpfCommandsFactory : ICommandsFactory
+    public interface IWpfCommandsFactory
     {
-        new IWpfCommand Create(Action execute, Func<bool>? canExecute = null);
-        new IWpfCommand Create<T>(Action<T> execute, Func<T, bool>? canExecute = null);
-        new IWpfCommand Create(Func<Task> execute, Func<bool>? canExecute = null);
-        new IWpfCommand Create<T>(Func<T, Task> execute, Func<T, bool>? canExecute = null);
+        IWpfCommand Create(Action execute, Func<bool>? canExecute = null);
+        IWpfCommand Create<T>(Action<T> execute, Func<T, bool>? canExecute = null);
+        IWpfCommand Create(Func<Task> execute, Func<bool>? canExecute = null);
+        IWpfCommand Create<T>(Func<T, Task> execute, Func<T, bool>? canExecute = null);
 
-        ICommand CreateBase(Action execute, Func<bool>? canExecute = null);
-        ICommand CreateBase<T>(Action<T> execute, Func<T, bool>? canExecute = null);
-        ICommand CreateBase(Func<Task> execute, Func<bool>? canExecute = null);
-        ICommand CreateBase<T>(Func<T, Task> execute, Func<T, bool>? canExecute = null);
-
+        ICommand CreateAsBase(Action execute, Func<bool>? canExecute = null);
+        ICommand CreateAsBase<T>(Action<T> execute, Func<T, bool>? canExecute = null);
+        ICommand CreateAsBase(Func<Task> execute, Func<bool>? canExecute = null);
+        ICommand CreateAsBase<T>(Func<T, Task> execute, Func<T, bool>? canExecute = null);
     }
 }
--- Thundire.MVVM.WPF/Commands/WpfCommandsFactory.cs
+++ Thundire.MVVM.WPF/Commands/WpfCommandsFactory.cs
@@ -1,8 +1,6 @@
-﻿using System.Threading.Tasks;
-using System;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
-using Thundire.MVVM.Core.Commands;
-using Thundire.MVVM.Core.Services;
 
 namespace Thundire.MVVM.WPF.Commands
 {
@@ -14,14 +12,9 @@ namespace Thundire.MVVM.WPF.Commands
         public IWpfCommand Create(Func<Task> execute, Func<bool>? canExecute = null) => new WpfAsyncCommand(execute, canExecute);
         public IWpfCommand Create<T>(Func<T, Task> execute, Func<T, bool>? canExecute = null) => new WpfAsyncCommand<T>(execute, canExecute);
 
-        public ICommand CreateBase(Action execute, Func<bool>? canExecute = null) => new WpfRelayCommand(execute, canExecute);
-        public ICommand CreateBase<T>(Action<T> execute, Func<T, bool>? canExecute = null) => new WpfRelayCommand<T>(execute, canExecute);
-        public ICommand CreateBase(Func<Task> execute, Func<bool>? canExecute = null)  => new WpfAsyncCommand(execute, canExecute);
-        public ICommand CreateBase<T>(Func<T, Task> execute, Func<T, bool>? canExecute = null) => new WpfAsyncCommand<T>(execute, canExecute);
-
-        IExecutableCommand ICommandsFactory.Create(Action execute, Func<bool>? canExecute) => Create(execute, canExecute);
-        IExecutableCommand ICommandsFactory.Create<T>(Action<T> execute, Func<T, bool>? canExecute) => Create(execute, canExecute);
-        IExecutableCommand ICommandsFactory.Create(Func<Task> execute, Func<bool>? canExecute) => Create(execute, canExecute);
-        IExecutableCommand ICommandsFactory.Create<T>(Func<T, Task> execute, Func<T, bool>? canExecute) => Create(execute, canExecute);
+        public ICommand CreateAsBase(Action execute, Func<bool>? canExecute = null) => new WpfRelayCommand(execute, canExecute);
+        public ICommand CreateAsBase<T>(Action<T> execute, Func<T, bool>? canExecute = null) => new WpfRelayCommand<T>(execute, canExecute);
+        public ICommand CreateAsBase(Func<Task> execute, Func<bool>? canExecute = null) => new WpfAsyncCommand(execute, canExecute);
+        public ICommand CreateAsBase<T>(Func<T, Task> execute, Func<T, bool>? canExecute = null) => new WpfAsyncCommand<T>(execute, canExecute);
     }
 }
