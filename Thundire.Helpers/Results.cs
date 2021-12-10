a7ebfd40c506d8043e1412edea73c9208867c5c3
Tarxos <ftarxos@gmail.com>
2021-12-10 17:41:47 +0300

    add Results models to Helpers project

---
+++ Thundire.Helpers/Results.cs
@@ -0,0 +1,18 @@
+﻿namespace Thundire.Helpers
+{
+    public abstract record Result
+    {
+        public static SuccessResult Ok() => new SuccessResult();
+        public static FailureResult Fail(params FailureDescription[] failures) => new FailureResult(failures);
+        public static SuccessResult<TResult> Ok<TResult>(TResult callBackData) => new SuccessResult<TResult>(callBackData);
+        public static ExitResult Exit { get; } = new ExitResult();
+    };
+
+    public record SuccessResult : Result;
+    public record SuccessResult<TResult>(TResult CallBackData) : SuccessResult;
+    public record FailureResult(params FailureDescription[] Failures) : Result;
+
+    public record ExitResult : Result;
+
+    public record FailureDescription(string Message, string Code);
+}
