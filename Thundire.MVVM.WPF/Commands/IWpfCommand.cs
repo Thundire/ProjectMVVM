66cb7e54cfcac13fa91aff797f1c3e31cd1816c3
Tarxos <ftarxos@gmail.com>
2021-12-10 13:55:09 +0300

    add base interface of wpf commands in MVVM.WPF project

---
+++ Thundire.MVVM.WPF/Commands/IWpfCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+using Thundire.MVVM.Core.Commands;
+
+namespace Thundire.MVVM.WPF.Commands
+{
+    public interface IWpfCommand : IExecutableCommand
+    {
+        public void RaiseCanExecuteChanged()
+        {
+            if (Application.Current.Dispatcher.CheckAccess())
+            {
+                CommandManager.InvalidateRequerySuggested();
+                return;
+            }
+            Application.Current.Dispatcher.Invoke(CommandManager.InvalidateRequerySuggested, DispatcherPriority.Send);
+        }
+    }
+}
