f5f84a66a651b081a7032a76b6ed1ee15d4a3401
Tarxos <ftarxos@gmail.com>
2021-12-10 13:56:26 +0300

    add wpf implementation of commands based on MVVM.Core realization

---
+++ Thundire.MVVM.WPF/Commands/WpfAsyncCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Thundire.MVVM.Core.Commands;
+
+namespace Thundire.MVVM.WPF.Commands
+{
+    public class WpfAsyncCommand<TParameter> : AsyncCommand<TParameter>, IWpfCommand, ICommand
+    {
+        private bool _executable = true;
+
+        public WpfAsyncCommand(Func<TParameter, Task> execute, Func<TParameter, bool>? canExecute = null, Action<Exception>? onException = null)
+            : base(execute, canExecute, onException)
+        {
+        }
+
+        public override bool Executable { get => _executable; set => this.Set(ref _executable, value, true); }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    public class WpfAsyncCommand : AsyncCommand, IWpfCommand, ICommand
+    {
+        private bool _executable = true;
+
+        public WpfAsyncCommand(Func<Task> execute, Func<bool>? canExecute = null, Action<Exception>? onException = null) : base(execute, canExecute, onException)
+        {
+        }
+
+        public override bool Executable { get => _executable; set => this.Set(ref _executable, value, true); }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ Thundire.MVVM.WPF/Commands/WpfRelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+using Thundire.MVVM.Core.Commands;
+
+namespace Thundire.MVVM.WPF.Commands
+{
+    public class WpfRelayCommand : RelayCommand, IWpfCommand, ICommand
+    {
+        private bool _executable = true;
+
+        public WpfRelayCommand(Action execute, Func<bool>? canExecute = null) : base(execute, canExecute)
+        {
+        }
+
+        public override bool Executable { get => _executable; set => this.Set(ref _executable, value, true); }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    public class WpfRelayCommand<TParameter> : RelayCommand<TParameter>, IWpfCommand, ICommand
+    {
+        private bool _executable = true;
+
+        public WpfRelayCommand(Action<TParameter> execute, Func<TParameter, bool>? canExecute = null) : base(execute, canExecute)
+        {
+        }
+
+        public override bool Executable { get => _executable; set => this.Set(ref _executable, value, true); }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
