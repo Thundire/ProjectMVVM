3907d1aa025f9685673d8d4b445900adf35de111
Tarxos <ftarxos@gmail.com>
2021-12-10 18:39:13 +0300

    replace observable collection extension method for AddRange

--- Thundire.MVVM.Core/Extensions.cs
+++ Thundire.MVVM.Core/Extensions.cs
@@ -7,5 +7,13 @@ namespace Thundire.MVVM.Core
     {
         public static ObservableCollection<T> ToObservable<T>(this IEnumerable<T>? param)
             => param is not null ? new(param) : new();
+
+        public static void AddRange<T>(this ObservableCollection<T> self, IEnumerable<T> range)
+        {
+            foreach (var item in range)
+            {
+                self.Add(item);
+            }
+        }
     }
 }
--- Thundire.MVVM.WPF/Extensions.cs
+++ Thundire.MVVM.WPF/Extensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 
@@ -11,15 +10,7 @@ namespace Thundire.MVVM.WPF
     {
         public static void DispatcherAdd<T>(this ObservableCollection<T> self, T item)
         {
-            Application.Current.Dispatcher.BeginInvoke( (Action)delegate { self.Add(item); });
-        }
-
-        public static void AddRange<T>(this ObservableCollection<T> self, IEnumerable<T> range)
-        {
-            foreach (var item in range)
-            {
-                self.Add(item);
-            }
+            Application.Current.Dispatcher.BeginInvoke((Action)delegate { self.Add(item); });
         }
 
         public static bool Set<T>(this IWpfCommand self, ref T field, T value, bool isExecutingDependsOnIt)
