0bf2b84b62e49a8b94b234fa92a51769082288d2
Tarxos <ftarxos@gmail.com>
2021-12-10 17:12:37 +0300

    correct name of DeepCopy extension method to JsonSerializationDeepCopy, and update for nullability

--- Thundire.Helpers/SerializationExtensions.cs
+++ Thundire.Helpers/SerializationExtensions.cs
@@ -4,6 +4,6 @@ namespace Thundire.Helpers
 {
     public static class SerializationExtensions
     {
-        public static T DeepCopy<T>(this T self) => JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(self));
+        public static T? JsonSerializationDeepCopy<T>(this T self) => JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(self));
     }
 }
--- Thundire.MVVM.WPF/Observable/EditForm/EditFormVM{TModel}.cs
+++ Thundire.MVVM.WPF/Observable/EditForm/EditFormVM{TModel}.cs
@@ -9,8 +9,8 @@ namespace Thundire.MVVM.WPF.Observable.EditForm
     public class EditFormVM<TModel> : EditFormVM where TModel : class, INotifyPropertyChanged, IEquatable<TModel>
     {
         // ReSharper disable InconsistentNaming
-        protected TModel _toEdit;
-        protected TModel _backup;
+        protected TModel? _toEdit;
+        protected TModel? _backup;
         // ReSharper restore InconsistentNaming
 
         protected EditFormVM(IWpfCommandsFactory commandsFactory) : base(commandsFactory)
@@ -18,13 +18,13 @@ namespace Thundire.MVVM.WPF.Observable.EditForm
             CancelCommand = commandsFactory.CreateAsBase(CancelExecute);
         }
 
-        public virtual TModel ToEdit
+        public virtual TModel? ToEdit
         {
             get => _toEdit;
             set
             {
                 _ = Set(ref _toEdit, value);
-                _backup ??= value.DeepCopy();
+                _backup ??= value.JsonSerializationDeepCopy();
             }
         }
 
@@ -35,7 +35,7 @@ namespace Thundire.MVVM.WPF.Observable.EditForm
                 EndWork(new() { Result = false });
                 return;
             }
-            ToEdit = _backup.DeepCopy();
+            ToEdit = _backup.JsonSerializationDeepCopy();
         }
 
         protected override void EndWork(EditFormResultArgs result)
