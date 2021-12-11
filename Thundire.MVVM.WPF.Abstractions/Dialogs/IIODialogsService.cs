b19512aeb05fd739028fabec082c26316f324515
Tarxos <ftarxos@gmail.com>
2021-12-11 18:29:49 +0300

    fix nullable in IODialogsService interface

--- Thundire.MVVM.WPF.Abstractions/Dialogs/IIODialogsService.cs
+++ Thundire.MVVM.WPF.Abstractions/Dialogs/IIODialogsService.cs
@@ -2,7 +2,7 @@
 {
     public interface IIODialogsService
     {
-        bool SaveFile(string title, out string selectedFile, string defaultFileName = null, string filter = "All files (*.*)|*.*");
+        bool SaveFile(string title, out string selectedFile, string? defaultFileName = null, string filter = "All files (*.*)|*.*");
         bool OpenFile(string title, out string selectedFile, string filter = "All files (*.*)|*.*");
     }
 }
