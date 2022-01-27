5ed983066331c40586a4b3ba7e1f80be924bb401
Tarxos <ftarxos@gmail.com>
2022-01-27 14:38:16 +0300

    add extension for enumerable to generate hashcode of that enumerable

--- Thundire.Helpers/GenericCollectionsExtensions.cs
+++ Thundire.Helpers/GenericCollectionsExtensions.cs
@@ -9,5 +9,22 @@ namespace System.Collections.Generic
         /// Validates that the <paramref name="enumerable"/> is not null and contains items.
         /// </summary>
         public static bool IsNotNullOrEmpty<T>(this IEnumerable<T>? enumerable) => enumerable is not null && enumerable.Any();
+
+        /// <summary>
+        /// Get hashcode from all members of enumerable and merge it
+        /// </summary>
+        /// <typeparam name="T">Item</typeparam>
+        /// <param name="enumerable">Enumerable of Items</param>
+        /// <returns>0 if enumerable is null or not contains not null values, if opposite returns merged hashcode of enumerable members</returns>
+        public static int GetSequenceHashCode<T>(this IEnumerable<T>? enumerable)
+        {
+            if (enumerable is null) return 0;
+            HashCode hash = new();
+
+            foreach (var value in enumerable.Where(v => v is not null))
+                hash.Add(value!.GetHashCode());
+
+            return hash.ToHashCode();
+        }
     }
 }
