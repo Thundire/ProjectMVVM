5f59976fb358d9a2b5cb9cbda4763d445a24b007
Tarxos <ftarxos@gmail.com>
2022-01-28 01:37:59 +0300

    add implementations of Services registration to microsoft di container

---
+++ DiContainers/Thundire.MVVM.MicrosoftDIContainer/MSContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Thundire.MVVM.WPF.Abstractions.DependencyInjection;
+
+namespace Thundire.MVVM.MicrosoftDIContainer
+{
+    public class MSContainer : IDIContainer
+    {
+        private readonly IServiceProvider _provider;
+
+        public MSContainer(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public object Resolve(Type type) => _provider.GetService(type);
+
+        public TService Resolve<TService>() => _provider.GetRequiredService<TService>();
+    }
+}
---
+++ DiContainers/Thundire.MVVM.MicrosoftDIContainer/MSContainerRegistrator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
+using Thundire.MVVM.WPF.Abstractions.DependencyInjection;
+
+namespace Thundire.MVVM.MicrosoftDIContainer
+{
+    public class MSContainerRegistrator : IDIContainerBuilder
+    {
+        private readonly IServiceCollection _services;
+
+        public MSContainerRegistrator(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public void RegisterType(Type type, LifeTimeMode mode)
+        {
+            if (mode == LifeTimeMode.Singleton)
+            {
+                _services.AddSingleton(type);
+                return;
+            }
+
+            _services.AddTransient(type);
+        }
+
+        public void RegisterTypeAs(Type type, Type asType, LifeTimeMode mode)
+        {
+            ServiceLifetime lifeTime = mode is LifeTimeMode.Singleton
+                ? ServiceLifetime.Singleton
+                : ServiceLifetime.Transient;
+
+            _services.Add(new ServiceDescriptor(asType, type, lifeTime));
+        }
+    }
+}
