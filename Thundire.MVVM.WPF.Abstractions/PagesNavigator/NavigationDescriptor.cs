e3a51cbd1fda2cb3d5c6f57b2f064c4e57707d2a
Tarxos <ftarxos@gmail.com>
2022-01-28 18:42:00 +0300

    add NavigationDescriptor

---
+++ Thundire.MVVM.WPF.Abstractions/PagesNavigator/NavigationDescriptor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Thundire.MVVM.WPF.Abstractions.PagesNavigator
+{
+    public class NavigationDescriptor
+    {
+        public int Order { get; init; }
+        public string PageKey { get; init; }
+        public string Title { get; init; }
+        public object? DataContext { get; set; }
+        public Type? DataContextType { get; init; }
+    }
+}
