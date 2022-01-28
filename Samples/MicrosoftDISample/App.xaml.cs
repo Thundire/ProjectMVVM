8e3914fe9d62006406287ee5c22cff5b9dd8fab2
Tarxos <ftarxos@gmail.com>
2022-01-28 21:18:40 +0300

    add injections of microsoft di container and project mvvm to microsoft di sample project

--- Samples/MicrosoftDISample/App.xaml.cs
+++ Samples/MicrosoftDISample/App.xaml.cs
@@ -1,8 +1,51 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Shared;
+
+using System;
+using System.Windows;
+
+using Thundire.MVVM.WPF.Abstractions.ViewService;
 
 namespace MicrosoftDISample
 {
     public partial class App : Application
     {
+        private static IHost? _hosting;
+        private static IHost Hosting => _hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await Hosting.StartAsync();
+
+            base.OnStartup(e);
+
+            var handler = Hosting.Services.GetRequiredService<IViewHandlerService>();
+            handler
+                .Search(ViewsKeys.Main)
+                .OnClose((sender, args) =>
+                {
+                    Shutdown();
+                    return default;
+                })
+                .Handle<IWindowView>()
+                ?.Show();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            using var host = Hosting;
+            await host.StopAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) => services
+                .RegisterServices()
+                .RegisterViewModels()
+                .RegisterViews());
     }
 }
---
+++ Samples/MicrosoftDISample/ContainerConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shared;
+using Shared.Services;
+using Shared.ViewModels;
+using Shared.ViewModels.Regions;
+using Shared.ViewModels.ViewService;
+using Shared.Views;
+using Shared.Views.Pages;
+using Thundire.MVVM.MicrosoftDIContainer;
+using Thundire.MVVM.WPF.Abstractions.Commands;
+using Thundire.MVVM.WPF.Commands;
+
+namespace MicrosoftDISample
+{
+    internal static class ContainerConfiguration
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection builder)
+        {
+            builder.AddSingleton<IWpfCommandsFactory, WpfCommandsFactory>();
+            builder
+                .AddSingleton<INavigationGroupDescriptors>(new NavigationGroupDescriptors()
+                    .AddDescriptor<FooVM>("Foo", "Foo")
+                    .AddDescriptor<BarVM>("Bar", "Bar")
+                    .AddDescriptor("Foo2", "Foo 2")
+                    .Build()
+                );
+            return builder;
+        }
+
+        public static IServiceCollection RegisterViewModels(this IServiceCollection builder)
+        {
+            builder.AddTransient<FooVM>();
+            builder.AddTransient<BarVM>();
+            builder.AddSingleton<SinglePageRegionTestMainVM>();
+            builder.AddSingleton<StackViewsRegionTestMainVM>();
+            builder.AddSingleton<RegionsMainVM>();
+            builder.AddSingleton<ViewOpenVM>();
+            builder.AddTransient<ConfirmVM>();
+
+            return builder;
+        }
+
+        public static IServiceCollection RegisterViews(this IServiceCollection builder)
+        {
+            builder.AddViewHandlerService(register =>
+            {
+                register.Register<MainWindow, MainVM>(ViewsKeys.Main);
+                register.Register<ConfirmWindow>(ViewsKeys.Confirm);
+                register.Register<NumbersEditorWindow, NumbersEditFormVM>(ViewsKeys.NumbersEditor);
+                register.Register<NavigationWindow, NavigationVM>(ViewsKeys.Navigation);
+            });
+
+            builder.AddRegionsService(cacheBuilder =>
+            {
+                cacheBuilder.FromResourceDictionary(new("Templates.xaml", UriKind.RelativeOrAbsolute));
+            }, regions => regions
+                    .RegisterSinglePageRegion(RegionsKeys.SinglePageRegion)
+                    .RegisterStackViewsRegion(RegionsKeys.StackViewsRegion));
+
+            builder.AddPages(registration =>
+            {
+                registration.AddGroup("NavigationGroup", pages =>
+                {
+                    pages.Register<FooPage>("Foo");
+                    pages.Register<FooPage>("Foo2");
+                    pages.Register<BarPage>("Bar");
+                });
+            });
+
+            return builder;
+        }
+    }
+}
